670b798bd99969616c6224b777fdbd776be8bbef
havaian <m.usman.work@gmail.com>
2025-07-29 12:57:30 +0500

    Fingers really crosseD now

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 // frontend/src/main.js
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createHead } from '@unhead/vue'
+import { injectHead } from '@unhead/vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
