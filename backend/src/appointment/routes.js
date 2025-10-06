9f67d35e5632c718d79603e8d025979207bfb0e0
havaian <m.usman.work@gmail.com>
2025-10-06 15:58:42 +0500

    Upload import problems fix

--- backend/src/appointment/routes.js
+++ backend/src/appointment/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('./controller');
 const { authenticateUser, authorizeRoles } = require('../auth');
-const upload = require('../middleware/upload');
+const upload = require('../utils/multerConfig');
 
 /**
  * @route POST /api/appointments
